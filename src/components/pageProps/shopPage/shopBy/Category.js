11e4f0a56ce4cfff78f25e506df8c1140627e607
saloxiddin04 <sayfuddinovsaloxiddin930@gmail.com>
2025-01-11 17:16:14 +0500

    change category

--- src/components/pageProps/shopPage/shopBy/Category.js
+++ src/components/pageProps/shopPage/shopBy/Category.js
@@ -8,30 +8,72 @@ const Category = () => {
   const items = [
     {
       _id: 990,
-      title: "New Arrivals",
-      icons: true,
+      title: "Рукавные фильтры с импульсной продувкой СРФ"
     },
     {
       _id: 991,
-      title: "Gudgets",
+      title: "Рукавные фильтры циклоны РЦИЭ",
     },
     {
       _id: 992,
-      title: "Accessories",
-      icons: true,
+      title: "Рукавные фильтры ФРИП",
     },
     {
       _id: 993,
-      title: "Electronics",
+      title: "Рукавные фильтры ФРКИ",
     },
     {
       _id: 994,
-      title: "Others",
+      title: "Рукавные фильтры ФРИ",
+    },
+    {
+      _id: 995,
+      title: "Рукавные фильтры ФРКН",
+    },
+    {
+      _id: 996,
+      title: "Рукавные фильтры с механическим встряхиванием РФУ",
+    },
+    {
+      _id: 997,
+      title: "Батарейный циклон БЦ",
+    },
+    {
+      _id: 997,
+      title: "Фильтровентиляционные установки",
+    },
+    {
+      _id: 999,
+      title: "Фильтры для силосов",
+    },
+    {
+      _id: 1000,
+      title: "Локальные фильтры",
+    },
+    {
+      _id: 1001,
+      title: "Двухступенчатые фильтры тонкой очистки воздуха СРФ-КР",
+    },
+    {
+      _id: 1002,
+      title: "Патронные фильтры СРФ-К",
+    },
+    {
+      _id: 1003,
+      title: "Автономные газоочистные установки",
+    },
+    {
+      _id: 1004,
+      title: "Рукавные фильтры",
+    },
+    {
+      _id: 1005,
+      title: "Масляный фильтр",
     },
   ];
   return (
     <div className="w-full">
-      <NavTitle title="Shop by Category" icons={false} />
+      <NavTitle title="Категория Продукта" icons={false} />
       <div>
         <ul className="flex flex-col gap-4 text-sm lg:text-base text-[#767676]">
           {items.map(({ _id, title, icons }) => (
